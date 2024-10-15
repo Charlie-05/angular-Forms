dabd9924fb2daa1c3e50b60d7b4c914a5d6d77eb
Paramananthan T. <UT01373@unicomtic.com>
2024-10-15 12:49:40 +0530

    Q1-Q4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators , AbstractControl , ValidationErrors , ValidatorFn, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,100 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+[x: string]: any;
   title = '2024-10-13';
+
+  user = {
+    email: '',
+    password: ''
+  }
+
+  customer = {
+    fullname: '',
+    gender: '',
+    country: ''
+  }
+
+  student = {
+    name: null as unknown as string,
+    dateOfBirth: '',
+    age: null as unknown as number
+  }
+
+  contactDetails = {
+    name: "",
+    email : '',
+    message : ''
+  }
+
+  contactSubmitted : boolean = false;
+
+  signUpForm : any ;
+  constructor(private fb : FormBuilder){
+    this.signUpForm = this.fb.group({
+      username : ['' , ],
+      password : [ '' ,[this.createPasswordStrengthValidator()] ]
+    })
+  }
+
+
+  // Custom validation function
+  createPasswordStrengthValidator(): ValidatorFn {
+    return (control:AbstractControl) : ValidationErrors | null => {
+
+        const value = control.value;
+
+        if (!value) {
+            return null;
+        }
+
+        const hasUpperCase = /[A-Z]+/.test(value);
+
+        const hasLowerCase = /[a-z]+/.test(value);
+
+        const hasNumeric = /[0-9]+/.test(value);
+
+        const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+
+        return !passwordValid ? {passwordStrength:true}: null;
+    }
+}
+NospaceValidator(): ValidatorFn {
+  return (control:AbstractControl) : ValidationErrors | null => {
+
+      const value = control.value;
+
+      if (!value) {
+          return null;
+      }
+
+      // const hasUpperCase = /[A-Z]+/.test(value);
+
+      // const hasLowerCase = /[a-z]+/.test(value);
+
+      // const hasNumeric = /[0-9]+/.test(value);
+
+      let strArray = value.split('');
+      strArray.forEach((x : string) => {
+        
+      });
+
+      //const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+
+     // return !passwordValid ? {passwordStrength:true}: null;
+  }
+}
+
+  onSubmit(logInform: any) {
+    console.log(logInform.value)
+  }
+  onRegister(registerForm: any) {
+    console.log(registerForm.value);
+    console.log(this.customer)
+  }
+
+  onContactSubmit(contactForm : any){
+    this.contactSubmitted = true;
+    console.log(contactForm.value)
+  }
 }
